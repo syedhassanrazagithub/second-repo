09f21b1b4b0085a40a76d67d1dd8712171ad3ff4
syed hassan raza <szada0795@gmail.com>
2024-08-23 10:36:59 -0700

    second trial

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let check = prompt("which table do you want to read")
+for(let i = 1; i <= 10; i++){
+    document.write( "<h1>" + check + " x " + i +  " = " + check*i + "</h1>" )
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
